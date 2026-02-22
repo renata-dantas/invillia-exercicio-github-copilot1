00cd3559505ec44d11ade6fe21a2bde79675de83
Renata Dantas <renata.dantas@everymind.uol>
2026-02-22 14:30:15 +0000

    Add extracurricular activities and signup validation to the api

--- src/app.py
+++ src/app.py
@@ -94,7 +94,7 @@ def signup_for_activity(activity_name: str, email: str):
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
-    # Get the specificy activity
+    # Get the specific activity
     activity = activities[activity_name]
 
     # Validate if the student is already signed up
